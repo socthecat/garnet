0721f023eb44052f2bda899a416a0819b93344da
socthecat <socthecat@gmail.com>
2019-05-25 17:43:16 +0300

    коміт для владіка

---
+++ src/components/WarrantyCard/WarrantyCard.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class WarrantyCard extends Component {
+    render() {
+        return (
+            <div>
+                ВЕРСТАТЬ ТУТ
+            </div>
+        );
+    }
+}
+
+export default WarrantyCard;
